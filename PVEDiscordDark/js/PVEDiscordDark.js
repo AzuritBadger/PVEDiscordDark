52a6aadaf46235fd1ac5441e5415bc74f9e51b91
AzuritBadger <64105171+AzuritBadger@users.noreply.github.com>
2021-09-03 21:03:01 +0000

    Change colors

--- PVEDiscordDark/js/PVEDiscordDark.js
+++ PVEDiscordDark/js/PVEDiscordDark.js
@@ -7,7 +7,7 @@ const swapLogo = async function() {
 			found = true;
 			var width = (node.parentElement.clientWidth == undefined || node.parentElement.clientWidth == 0) ? 172 : node.parentElement.clientWidth;
 			var height = (node.parentElement.clientHeight == undefined || node.parentElement.clientHeight == 0) ? 30 : node.parentElement.clientHeight;
-			node.parentElement.parentElement.style.background = '#23272A';
+			node.parentElement.parentElement.style.background = '#22272E';
 			node.setAttribute('height', `${height}px`);
 			node.setAttribute('width', `${width}px`);
 			node.setAttribute('src', '/pve2/images/dd_logo.png');
@@ -20,29 +20,29 @@ const swapLogo = async function() {
 };
 
 const patchCharts = function() {
-	Ext.chart.theme.Base.prototype.config.chart.defaults.background = '#23272a';
+	Ext.chart.theme.Base.prototype.config.chart.defaults.background = '#22272E';
 	Ext.chart.theme.Base.prototype.config.axis.defaults.label.color = 'white';
 	Ext.chart.theme.Base.prototype.config.axis.defaults.title.color = 'white';
-	Ext.chart.theme.Base.prototype.config.axis.defaults.style.strokeStyle = '#7289DA';
+	Ext.chart.theme.Base.prototype.config.axis.defaults.style.strokeStyle = '#539BF5';
 	Ext.chart.theme.Base.prototype.config.axis.defaults.grid.strokeStyle = 'rgba(44, 47, 51, 1)';
 	Ext.chart.theme.Base.prototype.config.sprites.text.color = 'white';
 };
 
 function patchGaugeWidget() {
-	Proxmox.panel.GaugeWidget.prototype.backgroundColor = '#2C2F33';
+	Proxmox.panel.GaugeWidget.prototype.backgroundColor = '#292E36';
 	Proxmox.panel.GaugeWidget.prototype.criticalColor = '#f04747';
 	Proxmox.panel.GaugeWidget.prototype.warningColor = '#faa61a';
-	Proxmox.panel.GaugeWidget.prototype.defaultColor = '#7289DA';
-	Proxmox.panel.GaugeWidget.prototype.items[1].series[0].colors[0] = '#2C2F33';
+	Proxmox.panel.GaugeWidget.prototype.defaultColor = '#539BF5';
+	Proxmox.panel.GaugeWidget.prototype.items[1].series[0].colors[0] = '#292E36';
 };
 
 function patchBackupConfig() {
-	PVE.window.BackupConfig.prototype.items.style['background-color'] = '#23272a';
+	PVE.window.BackupConfig.prototype.items.style['background-color'] = '#22272E';
 };
 
 function patchDiskSmartWindow() {
 	const target = PVE.DiskSmartWindow || Proxmox.window.DiskSmart;
-	target.prototype.items[1].style['background-color'] = '#23272a';
+	target.prototype.items[1].style['background-color'] = '#22272E';
 }
 
 function patchTFAEdit() {
@@ -53,7 +53,7 @@ function patchCreateWidget() {
 	_createWidget = Ext.createWidget
 	Ext.createWidget = function(c, p) {
 		if (typeof p === 'object' && typeof p.style === 'object') {
-			if (c === 'component' && typeof p.style['background-color'] === 'string' && p.style['background-color'] === 'white') p.style['background-color'] = '#2C2F33'
+			if (c === 'component' && typeof p.style['background-color'] === 'string' && p.style['background-color'] === 'white') p.style['background-color'] = '#292E36'
 		}
 		return _createWidget(c, p)
 	}
@@ -67,3 +67,5 @@ patchDiskSmartWindow();
 patchTFAEdit();
 patchCreateWidget();
 console.log('PVEDiscordDark :: Patched');
+
+
